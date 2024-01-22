6d0843ea2719d2a3d04ccbe9dab188cc6f3d3cd8
kolya <nikogham@pm.me>
2024-01-22 09:55:07 +0000

    change location request method

--- App.tsx
+++ App.tsx
@@ -1,13 +1,55 @@
-import { StyleSheet, Text, View } from 'react-native'
+import { Button, StyleSheet, Text, View } from 'react-native'
 import { BarCodeScanner } from 'expo-barcode-scanner'
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 import * as Location from 'expo-location'
+import * as TaskManager from 'expo-task-manager'
 import { LocationObject } from 'expo-location'
 import QrCodeScanner from './components/Screens/QrCodeScannerScreen/QrCodeScanner'
 import UrlDisplay from './components/Screens/QrCodeScannerScreen/UrlDisplay'
 import LoadingIndicator from './components/Screens/QrCodeScannerScreen/LoadingIndicator'
 import ScanAgainButton from './components/Screens/QrCodeScannerScreen/ScanAgainButton'
+
+const LOCATION_TASK_NAME = 'background-location-task'
+const requestPermissions = async () => {
+	try {
+		const { status: foregroundStatus } = await Location.requestForegroundPermissionsAsync()
+		if (foregroundStatus === 'granted') {
+			console.info(`foregroundLocationPermission: ${foregroundStatus}`)
+			const { status: backgroundStatus } = await Location.requestBackgroundPermissionsAsync()
+			if (backgroundStatus === 'granted') {
+				console.info(`backgroundLocationPermission: ${backgroundStatus}`)
+				await Location.startLocationUpdatesAsync(LOCATION_TASK_NAME, {
+					accuracy: Location.Accuracy.Highest,
+					timeInterval: 10000,
+					distanceInterval: 2,
+					activityType: 3,
+				})
+				console.info('background location services listening...')
+			}
+		}
+	} catch (error) {
+		console.error('Permissions Error:', error)
+	}
+}
+
+const PermissionsButton = () => (
+	<View>
+		<Button
+			onPress={requestPermissions}
+			title='Enable location services'
+		/>
+	</View>
+)
+
+//@ts-ignore
+TaskManager.defineTask(LOCATION_TASK_NAME, ({ data: { locations }, error }) => {
+	if (error) {
+		console.error('Task Manager Error:', error.message)
+		return
+	}
+	console.info(locations)
+})
 
 export default function App() {
 	const [hasPermission, setHasPermission] = useState<string | boolean>('not_requested')
@@ -20,14 +62,23 @@ export default function App() {
 	// Get User Permissions On App Launch
 	useEffect(() => {
 		;(async () => {
-			const { status } = await BarCodeScanner.requestPermissionsAsync()
-			setHasPermission(status === 'granted')
-		})()
-		;(async () => {
-			const { status } = await Location.requestForegroundPermissionsAsync()
-			if (status !== 'granted') {
-				setErrorMsg('Permission to location denied')
-				return
+			try {
+				const { status: permissionForUserCamera } = await BarCodeScanner.requestPermissionsAsync()
+				setHasPermission(permissionForUserCamera === 'granted')
+				if (permissionForUserCamera !== 'granted') {
+					setErrorMsg('Permission to camera denied')
+					return
+				}
+
+				// I'm acquring this on user launch. This is because it can take a few seconds to attain the location. If already attained, I can use the much quicker API call getLastKnownPositionAsync() when the user scans to reduce wait time.
+
+				// However this is going to require some testing to ensure getLastKnownPositionAsync() returns this location rather than an incorrect location.
+				const { status: foregroundStatus } = await Location.requestForegroundPermissionsAsync()
+				let location = await Location.getCurrentPositionAsync({})
+				setLocation(location)
+				console.info(location)
+			} catch (error) {
+				console.error(error)
 			}
 		})()
 	}, [])
@@ -39,7 +90,12 @@ export default function App() {
 	const onScan = async ({ type, data }: { type: string; data: string }): Promise<void> => {
 		setScanned(true)
 		setScanInProgress(true)
-		let location = await Location.getCurrentPositionAsync({})
+
+		let location = await Location.getLastKnownPositionAsync({})
+		if (!location) {
+			alert('Failed to attain location: please scan again')
+			return
+		}
 		setLocation(location)
 		let { latitude, longitude, altitude } = location.coords
 		try {
@@ -81,6 +137,7 @@ export default function App() {
 						scanned={scanned}
 						onScan={onScan}
 					/>
+					<PermissionsButton />
 					{scanned && (
 						<>
 							<UrlDisplay link={link} />
