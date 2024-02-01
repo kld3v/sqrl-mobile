d06d8d13b4c48dc00e278518aa77150ed45e3ca0
kolya <nikogham@pm.me>
2024-02-01 16:17:39 +0000

    implement basic camera

--- app/components/QrScanner.tsx
+++ app/components/QrScanner.tsx
@@ -3,6 +3,9 @@ import { StyleProp, TextStyle, View, ViewStyle } from "react-native"
 import { observer } from "mobx-react-lite"
 import { colors, typography } from "app/theme"
 import { Text } from "app/components/Text"
+import { Button } from "app/components/Button"
+
+import { Camera, CameraType } from "expo-camera"
 
 export interface QrScannerProps {
   /**
@@ -17,10 +20,32 @@ export interface QrScannerProps {
 export const QrScanner = observer(function QrScanner(props: QrScannerProps) {
   const { style } = props
   const $styles = [$container, style]
+  const [permission, requestPermission] = Camera.useCameraPermissions()
+
+  if (!permission) {
+    // Camera permissions are still loading
+    return <View />
+  }
+
+  if (!permission.granted) {
+    // Camera permissions are not granted yet
+    return (
+      <View style={$container}>
+        <Text style={$text}>We need your permission to show the camera</Text>
+        <Button onPress={requestPermission} text="requestPermission" />
+      </View>
+    )
+  }
 
   return (
     <View style={$styles}>
-      <Text style={$text}>Hello</Text>
+      <Camera
+        style={$camera}
+        type={CameraType.back}
+        onBarCodeScanned={(event) => {
+          console.log(event)
+        }}
+      />
     </View>
   )
 })
@@ -33,4 +58,9 @@ const $text: TextStyle = {
   fontFamily: typography.primary.normal,
   fontSize: 14,
   color: colors.palette.primary500,
+}
+
+const $camera: ViewStyle = {
+  height: "100%",
+  width: "100%",
 }
