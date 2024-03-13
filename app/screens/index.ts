98bf15f5715681eb47ddfa0e8204bf2d1b07194e
kolya <nikogham@pm.me>
2024-03-13 16:58:00 +0000

    Remove unused files and screens

--- app/screens/index.ts
+++ app/screens/index.ts
@@ -1,12 +1,6 @@
-export * from "./WelcomeScreen"
-
 export * from "./LoginScreen"
 export * from "./CommunityScreen"
 export * from "./DebugScreen/DebugScreen"
-export * from "./DemoShowroomScreen/MapScreen"
-
 export * from "./ErrorScreen/ErrorBoundary"
-// export other screens here
-export * from "./TestingScreen"
 export * from "./MarketPlaceScreen"
 export * from "./TermsAndConditionsScreen/TermsAndConditionsScreen"
