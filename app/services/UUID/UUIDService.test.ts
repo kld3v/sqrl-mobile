91ccf99c88b30b4701f67dd6017877d5c7a40e9b
kolyad3v <nikogham@pm.me>
2024-03-14 17:08:29 +0000

    Add Jest configuration to package.json

---
+++ app/services/UUID/UUIDService.test.ts
@@ -0,0 +1,11 @@
+import UUIDService from "./UUIDService"
+
+describe("UUIDService", () => {
+  it("should generate a valid UUID", () => {
+    const uuid = UUIDService.generateUUID()
+
+    expect(uuid).toMatch(
+      /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[1-5][0-9a-fA-F]{3}-[89abAB][0-9a-fA-F]{3}-[0-9a-fA-F]{12}$/,
+    )
+  })
+})
