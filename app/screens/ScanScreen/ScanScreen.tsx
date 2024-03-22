247cac67fd6e28b73f69cb74f738c63a6b9f321f
kolya <nikogham@pm.me>
2024-03-22 09:53:06 +0000

    bump v android

--- app/screens/ScanScreen/ScanScreen.tsx
+++ app/screens/ScanScreen/ScanScreen.tsx
@@ -1,12 +1,14 @@
 import { observer } from "mobx-react-lite"
 import React, { FC, useEffect } from "react"
-import { ViewStyle } from "react-native"
+import { View, ViewStyle } from "react-native"
+import { Text } from "../../components"
 
 import { QrVenueNotificationsManager, QrScanner, Screen } from "../../components"
 
 import { TabScreenProps } from "../../navigators/MainNavigator"
 import CameraPermissionDenied from "./CameraPermissionDenied"
 import { useCameraPermissions } from "expo-camera/next"
+import CameraPermissionUndetermined from "./CamerPermissionUndetermined"
 
 export const ScanScreen: FC<TabScreenProps<"Scan">> = observer(function ScanScreen(_props) {
   const [permission, requestPermission] = useCameraPermissions()
@@ -19,7 +21,13 @@ export const ScanScreen: FC<TabScreenProps<"Scan">> = observer(function ScanScre
   }, [permission, permission?.status])
 
   // Decide what to render based on the camera permission status
-  const content = permission?.granted ? <QrScanner /> : <CameraPermissionDenied />
+  const content = permission?.granted ? (
+    <QrScanner />
+  ) : permission?.status === "undetermined" ? (
+    <CameraPermissionUndetermined />
+  ) : (
+    <CameraPermissionDenied />
+  )
 
   return (
     <Screen preset="fixed" safeAreaEdges={["top"]} contentContainerStyle={$screenContentContainer}>
