25408758be320be0c6e1b65ff9b470d5cf4ce432
niko <nikogham@pm.me>
2024-03-27 12:54:04 +0000

    ios view fix

--- app/components/ScanScreenScore.tsx
+++ app/components/ScanScreenScore.tsx
@@ -16,29 +16,21 @@ export interface ScanScreenScoreProps {
  * Describe your component here
  */
 export const ScanScreenScore = observer(function ScanScreenScore(props: ScanScreenScoreProps) {
-  const { style } = props
-  const $styles = [$container, style]
   const { leaderboardStore } = useStores()
 
   return (
-    <View style={$styles}>
-      <View
-        style={{
-          width: "100%",
-          // backgroundColor: "blue",
-          position: "absolute",
-          zIndex: 3,
-          justifyContent: "center",
-          alignItems: "center",
-          top: spacing.xl,
-        }}
-      >
-        <Text text={leaderboardStore.score} />
-      </View>
+    <View
+      style={{
+        width: "100%",
+        // backgroundColor: "blue",
+        position: "absolute",
+        zIndex: 3,
+        justifyContent: "center",
+        alignItems: "center",
+        top: spacing.xl,
+      }}
+    >
+      <Text text={leaderboardStore.score} />
     </View>
   )
 })
-
-const $container: ViewStyle = {
-  justifyContent: "center",
-}
