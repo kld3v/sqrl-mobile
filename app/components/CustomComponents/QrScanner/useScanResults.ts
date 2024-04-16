bb922fe09898456dcab2d6509feda67379904e90
niko <nikogham@pm.me>
2024-04-16 10:41:38 +0100

    state

--- app/components/CustomComponents/QrScanner/useScanResults.ts
+++ app/components/CustomComponents/QrScanner/useScanResults.ts
@@ -10,7 +10,7 @@ export default () => {
   const [errorMsg, setErrorMsg] = useState<string | null>(null)
   const [safe, setSafe] = useState<boolean>(false)
   const [url, setUrl] = useState<string>("")
-  const [scanState, setScanState] = useState<ScanStateOptions>("scanned")
+  const [scanState, setScanState] = useState<ScanStateOptions>("notScanned")
   const readyToScan = useRef(true)
   const { locationStore, debugStore, leaderboardStore } = useStores()
 
